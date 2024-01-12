72e3cb83a8424518dc1230bbf18102caca41313b
ND3Silva <63899389+ND3Silva@users.noreply.github.com>
2024-01-12 14:45:23 +1100

    Add files via upload

---
+++ Q2/Q2/Q2.cpp
@@ -0,0 +1,14 @@
+// Q2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+string name;
+
+int main()
+{
+    cout << "What is your name?: ";
+    cin >> name;
+    cout << "Hello " << name << "!\n";
+    return 0;
+}
