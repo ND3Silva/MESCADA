bf5d6113636069e4ba79030facfb7c364a2971d3
ND3Silva <63899389+ND3Silva@users.noreply.github.com>
2024-01-12 14:23:13 +1100

    Add files via upload

---
+++ Q7/Q7/MultiplicationTable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q7
+{
+    using System;
+    using System.Net.Http.Headers;
+
+    public class MultiplicationTable
+    {
+        private int _number;
+
+        public MultiplicationTable(int SelectedNumber)
+        {
+            _number = SelectedNumber;
+        }
+
+        public void PrintTable()
+        {
+            int product;
+            Console.WriteLine("The Multiplication Table for " + _number + " is: ");
+            for (int i = 1; i <= 12; i++)
+            {
+                product = i * _number;
+                Console.WriteLine(i + "x" + _number + "= " + product + "\n");
+            }
+
+        }
+    }
+}
---
+++ Q7/Q7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Q7
+{
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            int n;
+            Console.WriteLine("Please enter a number");
+            n = Convert.ToInt32(Console.ReadLine());
+            MultiplicationTable MT = new(n);
+            MT.PrintTable();
+
+        }
+
+    }
+
+}
+
