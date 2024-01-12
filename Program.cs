1b0aa2391546f106e777125ddfeb4c29573f983c
ND3Silva <63899389+ND3Silva@users.noreply.github.com>
2024-01-12 14:10:38 +1100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Q4
+{
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            
+            int n;
+            Console.WriteLine("Please enter a number");
+            n=Convert.ToInt32(Console.ReadLine());
+            Sum UserSum = new(n);
+            UserSum.PrintSum();
+      
+   
+        }
+
+    }
+
+}
+
