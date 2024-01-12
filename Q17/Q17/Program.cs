357c6eec72cfcb196c0fd41ea62bfb59fc84fa76
ND3Silva <63899389+ND3Silva@users.noreply.github.com>
2024-01-12 16:27:09 +1100

    Add files via upload

---
+++ Q17/Q17/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography;
+namespace Q17
+{
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+
+            List<int> list1 = new List<int> { 2342 };
+
+            int num = list1[0];
+            int[] result =  new int[4];
+            int count = 0;
+            while (num > 0)
+            {
+                int digit = num % 10;
+                result[count] = digit;
+                num =num / 10;
+                count++;
+            }
+            foreach (int n in result)
+            {
+                Console.WriteLine(n);
+            };
+
+
+
+        }
+
+    }
+
+}
+
