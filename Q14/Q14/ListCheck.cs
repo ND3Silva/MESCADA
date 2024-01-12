f1053a8ec65a3b1c2f42961fa1070a27e150edd2
ND3Silva <63899389+ND3Silva@users.noreply.github.com>
2024-01-12 16:24:16 +1100

    Add files via upload

---
+++ Q14/Q14/ListCheck.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q14
+{
+    using System;
+    using System.Collections;
+    using System.Net.Http.Headers;
+
+    public class ListSum
+    {
+        private List<int> _list;
+
+        public ListSum(List<int> InputList)
+        {
+            _list = InputList;
+        }
+
+        public void ComputeSumForLoop()
+        {
+            int sum = 0;
+
+            for (int i = 0; i < _list.Count; i++)
+            {
+                sum += _list[i];
+
+            }
+            Console.WriteLine("The Sum using For Loop is: "+sum);
+
+        }
+        public void ComputeSumWhileLoop()
+        {
+            int Count= 0;
+            int sum = 0;
+            while (Count <= _list.Count-1)
+            {
+                sum += _list[Count];
+                Count++;
+            }
+
+            Console.WriteLine("The Sum using While loop is: "+ sum);
+
+        }
+       public int ComputeSumRecursion(List<int> list, int size)
+        {
+            if (size == 0)
+            {  // final recursion
+                return 0;
+            }
+
+            return (ComputeSumRecursion(list, size - 1)+ list[size-1]); 
+          
+        }
+    }
+}
---
+++ Q14/Q14/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography;
+namespace Q14
+{
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            List<int> inputList = new List<int> { 1, 2, 3, 4, 5 } ;
+            ListSum SumList = new ListSum(inputList);
+
+            SumList.ComputeSumForLoop();
+            SumList.ComputeSumWhileLoop();
+         Console.WriteLine("The Sum Using recursion is: " + SumList.ComputeSumRecursion(inputList,inputList.Count));
+
+
+
+
+        }
+
+    }
+
+}
+
