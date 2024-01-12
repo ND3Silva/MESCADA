d60219a34d7397d0d74ec0b9ba6b3cded056c008
ND3Silva <63899389+ND3Silva@users.noreply.github.com>
2024-01-12 16:23:22 +1100

    Add files via upload

---
+++ Q13/Q13/ListCheck.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q13
+{
+    using System;
+    using System.Collections;
+    using System.Net.Http.Headers;
+
+    public class WordCheck
+    {
+        private string _word;
+
+        public WordCheck(string InputWord)
+        {
+            _word = InputWord;
+        }
+
+        public void CheckPalindrome()
+        {
+
+            int SameElementCount=0;
+            for (int i=0;i<(_word.Length/2);i++)
+            {
+
+                for (int j=_word.Length-1;j>=_word.Length/2;j--) 
+                {
+                    if (_word[i] == _word[j])
+                    {
+                        SameElementCount++;
+                        break;
+                    }
+
+                }
+            }
+
+            if( SameElementCount == _word.Length/2 ) {
+
+                Console.WriteLine(" The word is a palindrome ");
+               
+            }
+            else
+            {
+                Console.WriteLine(" The word is not a palindrome ");
+                
+            }
+           
+
+        }
+        
+    }
+}
---
+++ Q13/Q13/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Cryptography;
+namespace Q13
+{
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            string input;
+          
+            Console.WriteLine("Please enter a word: ");
+            input = Console.ReadLine();
+            WordCheck Word = new WordCheck(input) ;
+            Word.CheckPalindrome();
+           
+
+        }
+
+    }
+
+}
+
