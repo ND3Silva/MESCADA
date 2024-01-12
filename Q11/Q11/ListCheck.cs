a1b9ce19becf610a0f9a603889d30012646f8f7e
ND3Silva <63899389+ND3Silva@users.noreply.github.com>
2024-01-12 14:27:07 +1100

    Add files via upload

---
+++ Q11/Q11/ListCheck.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q11
+{
+    using System;
+    using System.Net.Http.Headers;
+
+    public class ListCheck
+    {
+        private List<int> _list;
+
+        public ListCheck(List<int> InputList)
+        {
+            _list = InputList;
+        }
+        public void CheckElementOccurs(int Element)
+        {
+            Console.WriteLine("You are checking for Element: " + Element + " in the list");
+            if (_list.Contains(Element))
+            {
+                Console.WriteLine("\n" + Element + " is in the list in position: " + _list.IndexOf(Element));
+
+            }
+            else { Console.WriteLine("The element is not found in the list"); }
+
+      
+        }
+        
+    }
+}
---
+++ Q11/Q11/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Q11
+{
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            int n;
+          
+            Console.WriteLine("Please enter a list of 5 numbers one by one: ");
+            List<int> UserInputList = new List<int> ();
+            for (int i =0; i<=5; i++)
+            {
+                n = Convert.ToInt32(Console.ReadLine());
+                UserInputList.Add(n);
+            }
+          
+            ListCheck List = new(UserInputList);
+            Console.WriteLine("What Element do you want to check for? ");
+            int QueryElement = Convert.ToInt32(Console.ReadLine()); 
+            List.CheckElementOccurs(QueryElement);
+
+        }
+
+    }
+
+}
+
