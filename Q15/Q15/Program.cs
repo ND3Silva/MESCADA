a426c4103ec553a35887c32ad90fedfaa5916bf6
ND3Silva <63899389+ND3Silva@users.noreply.github.com>
2024-01-12 16:25:01 +1100

    Add files via upload

---
+++ Q15/Q15/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security.Cryptography;
+namespace Q15
+{
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+           List<string> list1  = new List<string >{ "a", "b", "c"};
+            List<int> list2 = new List<int> { 1, 2, 3 };
+            List<string> Concat = new List<string> ();
+
+
+            foreach (string s in list1) 
+            {
+                
+             Concat.Add(s); }
+
+            foreach (int n in list2)
+            {
+                string s = n.ToString(); 
+                Concat.Add(s);
+            }
+
+
+            foreach(string s in Concat)
+            {
+                Console.WriteLine(s);
+            }
+
+
+
+
+        }
+
+    }
+
+}
+
