904974fc6f8a5aec6272de3928064df8eadc2a5d
ND3Silva <63899389+ND3Silva@users.noreply.github.com>
2024-01-12 14:26:18 +1100

    Add files via upload

---
+++ Q10/Q10/ListCheck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q10
+{
+    using System;
+    using System.Net.Http.Headers;
+
+    public class ListCheck
+    {
+        private List<int> _list;
+
+        public ListCheck(List<int> InputList)
+        {
+            _list = InputList;
+        }
+
+        public int ReturnLargestElement()
+        {
+            Console.WriteLine("The Largest Element in the list is: " + _list.Max());
+            return _list.Max();
+
+        }
+        
+    }
+}
