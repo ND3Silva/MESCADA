57939d2870942df2f854e9de0555527d7a1a5de3
ND3Silva <63899389+ND3Silva@users.noreply.github.com>
2024-01-12 14:24:44 +1100

    Add files via upload

---
+++ Q9/Q9/LeapYear.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q9
+{
+    using System;
+    using System.Net.Http.Headers;
+
+    public class LeapYear
+    {
+        private int _InputYear;
+
+        public LeapYear(int CurrentYear)
+        {
+            _InputYear = CurrentYear;
+        }
+
+        public bool IsLeapYear(int year)
+        {
+            return ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
+            // checking if year can be divided by 4 but also cannot be divided by 100, but including division by 400
+            // every 4 years is  a leap year but not every 100 years, except for exception of  every 400 years.
+        }
+
+        public void PrintNext20LeapYears()
+        {
+            int LeapCount = 0;
+            Console.WriteLine("The Next 20 Leap years after the given year " + _InputYear + " is: ");
+            // computing next 20 not including the input year
+            for (int CrntYear = _InputYear+1; LeapCount <= 20; CrntYear++)
+            {
+                if (IsLeapYear(CrntYear))
+                {
+                    Console.WriteLine(CrntYear + "\n");
+                    LeapCount++;
+                }
+
+            }
+
+
+        }
+    }
+}
---
+++ Q9/Q9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Q9
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+           
+            int n;
+            Console.WriteLine("What year is it?: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            LeapYear LY = new LeapYear(n);
+            LY.PrintNext20LeapYears();
+
+        }
+
+    }
+
+}
+
