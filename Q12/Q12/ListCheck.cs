f7e98a3e01bd7ced6bc8be6b32224effdb863a87
ND3Silva <63899389+ND3Silva@users.noreply.github.com>
2024-01-12 14:31:09 +1100

    Add files via upload

---
+++ Q12/Q12/ListCheck.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q12
+{
+    using System;
+    using System.Net.Http.Headers;
+
+    public class ListCheck
+    {
+        private List<int> _list;
+
+        public ListCheck(List<int> InputList)
+        {
+            _list = InputList;
+        }
+
+        public List<int> ReturnOddElements()
+        {
+            List<int> OddElements = new List<int>();
+
+            foreach (int i in _list)
+            {
+                if (i % 2 != 0 || i==0)  //checking for odd element index and adding to new list (starting at 0)
+                {
+                    OddElements.Add(_list[i]);
+                }
+            }
+
+
+            Console.WriteLine("The Elements in odd positions in the list are: ");
+
+            OddElements.ForEach(i => Console.WriteLine(i));
+            foreach(int y in OddElements)
+            {
+                Console.WriteLine(OddElements);
+            }
+
+            return OddElements;
+
+        }
+        
+    }
+}
---
+++ Q12/Q12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Q12
+{
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            int n;
+          
+            Console.WriteLine("Please enter a list of 5 numbers one by one: ");
+            List<int> UserInputList = new List<int> ();
+            for (int i =0; i<=5; i++)
+            {
+                n = Convert.ToInt32(Console.ReadLine());
+                UserInputList.Add(n);
+            }
+          
+            ListCheck List = new(UserInputList);
+            List.ReturnOddElements();
+
+        }
+
+    }
+
+}
+
