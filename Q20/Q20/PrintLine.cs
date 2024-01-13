9bfbdb1357027f998d001743f04f14e644dc0616
ND3Silva <63899389+ND3Silva@users.noreply.github.com>
2024-01-13 20:07:28 +1100

    Add files via upload

---
+++ Q20/Q20/PrintLine.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q18
+{
+    using System;
+    using System.Collections;
+    using System.Net.Http.Headers;
+
+    public class PrintLine
+    {
+        private List<string> _list;
+
+        public PrintLine(List<string> InputList)
+        {
+            _list = InputList;
+        }
+
+        public void RearrangeToFixedChar(List<string> list, int CharCount)
+        {
+            //keeping track of characters
+            int CurrentCharsInLine = 0;
+
+          foreach( string word in list)
+          {
+                //adjusting output if chars are larger than required line amount
+                if(CurrentCharsInLine+ word.Length+1> CharCount)
+                {
+                    Console.WriteLine();
+                    CurrentCharsInLine = 0;
+                }
+
+                //include spaces for sentences and count
+                Console.Write(word+" ");
+                CurrentCharsInLine += word.Length+1;
+
+          }
+          
+         
+
+        }
+
+    }
+       
+    
+}
---
+++ Q20/Q20/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+namespace Q18
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            List<string> list1 = new List<string> { "This", "Text", "is","very","long", "for", "a", "string"};
+
+            PrintLine list = new PrintLine(list1);
+
+            list.RearrangeToFixedChar(list1, 10);
+        }
+
+    }
+
+}
+
