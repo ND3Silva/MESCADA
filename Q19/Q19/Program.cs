ba9a766d08926006a39f1d96294261b4aa1d59cd
ND3Silva <63899389+ND3Silva@users.noreply.github.com>
2024-01-13 20:06:16 +1100

    Add files via upload

---
+++ Q19/Q19/Program.cs
@@ -0,0 +1,26 @@
+﻿using Q19;
+using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+namespace Q19
+{
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            List<string> list1 = new List<string> { "The", "Quick", "Brown","Fox"};
+            List<string> list2 = new List<string> { "hetay", "uickqay", "rownbay", "oxfay" };
+            Translate Sentence = new Translate(list1);
+            Console.WriteLine("Translating "+ "'The Quick Brown Fox'"+" into Pig Latin: ");
+            Sentence.TranslateToPigLatin();
+            Console.WriteLine("Translating Back into English: ");
+            Sentence.TranslateToEnglish(list2);
+
+        }
+
+    }
+
+}
+
---
+++ Q19/Q19/Translate.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q19
+{
+    using System;
+    using System.Collections;
+    using System.ComponentModel.DataAnnotations;
+    using System.Net.Http.Headers;
+
+    public class Translate
+    {
+        private List<string> _list;
+
+        public Translate(List<string> InputList)
+        {
+            _list = InputList;
+        }
+
+        public void TranslateToPigLatin()
+        {
+
+            foreach (string word in _list)
+            {
+                char FL = word.First();
+                string firstLetter = FL.ToString();
+                string WordWithoutFirstLetter = word.Substring(1);
+
+                Console.WriteLine(WordWithoutFirstLetter + firstLetter.ToLower() + "ay");
+            }
+
+        }
+
+        public string WordWithoutSuffix(string word){
+            //removing last 2 characters from word
+            return word.Substring(0, word.Length - 2);
+        }
+
+        public string BringLastLetterToFrontAndRemove(string word)
+        {
+            //saving last letter, moving and removing from word
+            char lastletter = word.Last();
+            word = lastletter + word;
+            word= word.Substring(0, word.Length - 1);
+            return word;
+
+        }
+        public void TranslateToEnglish(List<string> list)
+        {
+            for( int i =0; i<list.Count; i++)
+            {
+                if (list[i].EndsWith("ay"))
+                {   //satisfy condition then remove suffix, swap and remove letters
+                    list[i] = WordWithoutSuffix(list[i]);
+                    list[i] = BringLastLetterToFrontAndRemove(list[i]);
+                    Console.WriteLine(list[i]);
+                    
+                     
+                }
+
+            }
+
+        }
+
+    }
+       
+    
+}
