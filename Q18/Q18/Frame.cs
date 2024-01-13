7f8926a23ee17622fd7e318c5c82d5459e9e5850
ND3Silva <63899389+ND3Silva@users.noreply.github.com>
2024-01-13 20:05:08 +1100

    Add files via upload

---
+++ Q18/Q18/Frame.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q18
+{
+    using System;
+    using System.Collections;
+    using System.Net.Http.Headers;
+
+    public class Frame
+    {
+        private List<string> _list;
+
+        public Frame(List<string> InputList)
+        {
+            _list = InputList;
+        }
+
+        public void MakeFrame()
+        {
+            int squareFrameWidth = 0;
+
+            //finding largest word to make border length
+            foreach (string word in _list)
+            {
+                if (word.Length > squareFrameWidth)
+                {
+                    squareFrameWidth = word.Length;
+                }
+            }
+            //writing top border with extra stars for the vertical lines
+            string border = new string('*', squareFrameWidth + 2);
+            Console.WriteLine(border);
+
+            //writing each word and padding to reach the largest word length
+            foreach (string s in _list)
+            {
+                Console.WriteLine("*" + s.PadRight(squareFrameWidth) + "*");
+            }
+
+            //bottom border
+            Console.WriteLine(border);
+
+        }
+
+    }
+       
+    
+}
---
+++ Q18/Q18/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+namespace Q18
+{
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            List<string> list1 = new List<string> { "Hello", "World", "in","a","frame"};
+
+            Frame list = new Frame(list1);
+            list.MakeFrame();
+        }
+
+    }
+
+}
+
