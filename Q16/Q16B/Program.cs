e17a7248dea0e633eda95cd713482e14e618f230
ND3Silva <63899389+ND3Silva@users.noreply.github.com>
2024-01-14 13:17:06 +1100

    Add files via upload

---
+++ Q16/Q16B/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO.IsolatedStorage;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Q16
+{
+
+    class MainClass
+    {
+        
+        public static void Main(string[] args)
+        {          
+            List<int> list1 = new List<int> { 1,2,3,5,6,};
+            List<string> list2 = new List<string> { "a","b","c"};
+            List<int> list3 = new List<int> { 1, 2, 3};
+            ChangeElements Input = new ChangeElements(list1);
+
+           int MoveBy2Elements = 2;
+           Input.MoveElementsByK(list1, MoveBy2Elements);
+
+           Console.WriteLine(string.Join(",",Input.InterchangeElements(list2, list3)));
+          
+        }
+        
+
+    }
+
+}
+
---
+++ Q16/Q16B/Translate.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q16
+{
+    public class ChangeElements
+    {
+        private List<int> _list;
+
+        public ChangeElements(List<int> InputList)
+        {
+            _list = InputList;
+        }
+
+        public void MoveElementsByK(List<int> list1, int k)
+        {
+            int NewIndex = 0;
+            int[] temp = new int[list1.Capacity];
+
+            //start by saving values for 0-k indeces
+            for (int j = 0; j <= (k); j++)
+            {
+                temp[j] = list1[j];
+            }
+            // bring kth index to 0 and then start sequence again
+            for (int i = k; i < list1.Count; i++)
+            {
+                list1[NewIndex] = list1[i];
+                NewIndex++;
+            }
+            for (int j = 0; j < (k); j++)
+            {
+                // Take final values stored earlier in temp and put at the end of list
+                list1[NewIndex] = temp[j];
+                NewIndex++;
+            }
+            list1.ForEach(Console.WriteLine);
+
+        }
+
+        public List<string> InterchangeElements(List<string> List1, List<int> List2)
+        {
+            int combinedLength = List1.Count + List2.Count;
+            List<string> result = new List<string>();
+
+            for (int i=0; i<combinedLength; i++)
+            {
+                //checking the bounds whether it can handle the no. of elements
+                if (i < List1.Count)
+                {
+                    result.Add(List1[i].ToString());
+                }
+
+                if (i < List2.Count)
+                {
+                    result.Add(List2[i].ToString());
+                }
+
+            }
+
+            return result;
+        }
+
+
+
+
+
+
+    }
+       
+    
+}
